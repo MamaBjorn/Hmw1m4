2e92a01959678c04fb2cd0b85d1ad3db36b6a02d
MamaBjorn <3320135@gmail.com>
2023-09-23 12:12:36 +1000

    Initial commit

---
+++ src/oo/Counter.java
@@ -0,0 +1,12 @@
+package oo;
+
+public class Counter {
+    public static int count;
+    public void increment(){
+        count++;
+
+    }
+    public static int getValue(){
+        return count;
+    }
+}
